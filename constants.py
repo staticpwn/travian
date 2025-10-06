c788d694575921f9154a81bdf69377df29f28a66
staticpwn <101466946+staticpwn@users.noreply.github.com>
2025-10-06 15:56:40 +0400

    Upload file via API

---
+++ constants.py
@@ -0,0 +1,60 @@
+import pyautogui as pa
+
+tween_functions = [
+    pa.linear,         # constant speed
+    pa.easeInQuad,     # slow start, accelerate
+    pa.easeOutQuad,    # fast start, decelerate
+    pa.easeInOutQuad,  # accelerate then decelerate
+    pa.easeInCubic,
+    pa.easeOutCubic,
+    pa.easeInOutCubic,
+    pa.easeInQuart,
+    pa.easeOutQuart,
+    pa.easeInOutQuart,
+    pa.easeInQuint,
+    pa.easeOutQuint,
+    pa.easeInOutQuint,
+    pa.easeInSine,
+    pa.easeOutSine,
+    pa.easeInOutSine,
+    pa.easeInExpo,
+    pa.easeOutExpo,
+    pa.easeInOutExpo,
+    pa.easeInCirc,
+    pa.easeOutCirc,
+    pa.easeInOutCirc,
+    pa.easeInElastic,
+    pa.easeOutElastic,
+    pa.easeInOutElastic,
+    pa.easeInBack,
+    pa.easeOutBack,
+    pa.easeInOutBack,
+    pa.easeInBounce,
+    pa.easeOutBounce,
+    pa.easeInOutBounce
+]
+
+accounts = {
+    "NobelSword": {
+        "email": "mohammad.wissam.farhoud@hotmail.com",
+        "password": "express_19915",
+        "current": True,
+    },
+    "daggerman": {
+        "email": "farhoud.solutions@gmail.com",
+        "password": "aboodf2000",
+        "current": False,
+    }
+}
+
+target_urls = {
+    "login": "https://www.travian.com/international#loginLobby",
+    "account": "https://lobby.legends.travian.com/account",
+    "tile_base": "https://ts2.x1.international.travian.com/karte.php?", # add f"x={x_coord}&y={y_coord}""
+    "terana_village": "https://ts2.x1.international.travian.com/dorf2.php?newdid=31591", #must navigate to terana after login before starting to check troops
+    "send_troops": "https://ts2.x1.international.travian.com/build.php?id=39&gid=16&tt=2",
+}
+
+TTT_BOX_NUMBER = 1
+TTT_RAID_COUNT = 5
+RAIDING_RADIUS_FROM_TERANA = 35
